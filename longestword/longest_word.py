76d440e15b59dcf0d1755f08885e1c4825f4a8f4
JobM <jobmwaniki18@gmail.com>
2015-11-18 12:23:41 +0300

    added longest word python

---
+++ longestword/longest_word.py
@@ -0,0 +1,4 @@
+def longest_word(sentence):
+#     return sorted(sentence.split(" "), key=len, reverse=True)[0]
+#     #return max([word for word in sentence.split(" ")],key=len)
+ print(longest_word("Hello there bro how was you lovely day"))
